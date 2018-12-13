763f3e134b013ab153b0308e05f137372c5ccc7c
BoredomIncarnate <dev.kyoung@gmail.com>
2018-12-12 17:14:14 -0800

    updated script to include reading the guess

--- utp.sh
+++ utp.sh
@@ -2,9 +2,10 @@
 id=`curl --silent "https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0" | jq '.query.random[].id'`
 echo "the id is $id"
 title=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].title' | sed "s/\"//g"`
-extract=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].extract'`
+extract=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].extract' | sed "s/\"//g"`
 echo "The title is: $title"
-echo "The extract is: $extract"
 replace="XXXXX"
-newExtract=`sed "s/$title/$replace/g" <<< "$extract"`
+newExtract=`sed "s/$title/$replace/Ig" <<< "$extract"`
 echo $newExtract
+read -p "Input guess: " guess
+echo $guess
