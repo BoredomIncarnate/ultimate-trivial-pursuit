b09228ffea8ba49501e3af4162ac818c1dc8dfbf
BoredomIncarnate <dev.kyoung@gmail.com>
2018-12-12 16:59:32 -0800

    basic replacement implemented

--- utp.sh
+++ utp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 id=`curl --silent "https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0" | jq '.query.random[].id'`
 echo "the id is $id"
-title=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].title'`
+title=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].title' | sed "s/\"//g"`
 extract=`curl --silent "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=$id" | jq '.query.pages[].extract'`
 echo "The title is: $title"
 echo "The extract is: $extract"
-#title=`jq --arg $article ".query.pages[].title"
+replace="XXXXX"
+newExtract=`sed "s/$title/$replace/g" <<< "$extract"`
+echo $newExtract
